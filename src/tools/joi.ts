a7182db20b0b3190fe98aed637b4cfbe2cb58bb5
bakjonghun <bakjonghun@hikick.kr>
2021-05-03 19:22:56 +0900

    change: joi stripUnknown to true

--- src/tools/joi.ts
+++ src/tools/joi.ts
@@ -151,7 +151,7 @@ const messages = {
 };
 
 const Joi = JoiLib.defaults((schema) =>
-  schema.options({ abortEarly: true, messages })
+  schema.options({ stripUnknown: true, messages })
 );
 
 export default Joi;
